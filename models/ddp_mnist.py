c26d2063e53ec4ebeab9b42185d6525a9c5f0e34
Edward <anqiyuanms@hotmail.com>
2022-02-07 21:07:50 -0600

    rank = args.nr * args.gpus + gpu

--- models/ddp_mnist.py
+++ models/ddp_mnist.py
@@ -12,11 +12,28 @@ from apex.parallel import DistributedDataParallel as DDP
 from apex import amp
 
 
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', '--nodes', default=1, type=int, metavar='N',
+                        help='number of data loading workers (default: 4)')
+    parser.add_argument('-g', '--gpus', default=1, type=int,
+                        help='number of gpus per node')
+    parser.add_argument('-nr', '--nr', default=0, type=int,
+                        help='ranking within the nodes')
+    parser.add_argument('--epochs', default=2, type=int, metavar='N',
+                        help='number of total epochs to run')
+    args = parser.parse_args()
+    args.world_size = args.gpus * args.nodes
+    os.environ['MASTER_ADDR'] = '10.57.23.164'
+    os.environ['MASTER_PORT'] = '8888'
+    mp.spawn(train, nprocs=args.gpus, args=(args,))
+
+
 class ConvNet(nn.Module):
     def __init__(self, num_classes=10):
         super(ConvNet, self).__init__()
         self.layer1 = nn.Sequential(
-            nn.Conv2d(3, 16, kernel_size=5, stride=1, padding=2),
+            nn.Conv2d(1, 16, kernel_size=5, stride=1, padding=2),
             nn.BatchNorm2d(16),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2))
@@ -25,7 +42,7 @@ class ConvNet(nn.Module):
             nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2))
-        self.fc = nn.Linear(2048, num_classes)
+        self.fc = nn.Linear(7*7*32, num_classes)
 
     def forward(self, x):
         out = self.layer1(x)
@@ -35,20 +52,9 @@ class ConvNet(nn.Module):
         return out
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-n', '--nodes', default=1, type=int, metavar='N')
-    parser.add_argument('-g', '--gpus', default=1, type=int,
-                        help='number of gpus per node')
-    parser.add_argument('-nr', '--nr', default=0, type=int,
-                        help='ranking within the nodes')
-    parser.add_argument('--epochs', default=2, type=int, metavar='N',
-                        help='number of total epochs to run')
-    args = parser.parse_args()
-    train(0, args)
-
-
 def train(gpu, args):
+    rank = args.nr * args.gpus + gpu
+    dist.init_process_group(backend='nccl', init_method='env://', world_size=args.world_size, rank=rank)
     torch.manual_seed(0)
     model = ConvNet()
     torch.cuda.set_device(gpu)
@@ -57,16 +63,22 @@ def train(gpu, args):
     # define loss function (criterion) and optimizer
     criterion = nn.CrossEntropyLoss().cuda(gpu)
     optimizer = torch.optim.SGD(model.parameters(), 1e-4)
+    # Wrap the model
+    model = nn.parallel.DistributedDataParallel(model, device_ids=[gpu])
     # Data loading code
-    train_dataset = torchvision.datasets.CIFAR10(root='./',
-                                                 train=True,
-                                                 transform=transforms.ToTensor(),
-                                                 download=True)
+    train_dataset = torchvision.datasets.MNIST(root='./data',
+                                               train=True,
+                                               transform=transforms.ToTensor(),
+                                               download=True)
+    train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset,
+                                                                    num_replicas=args.world_size,
+                                                                    rank=rank)
     train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
                                                batch_size=batch_size,
-                                               shuffle=True,
+                                               shuffle=False,
                                                num_workers=0,
-                                               pin_memory=True)
+                                               pin_memory=True,
+                                               sampler=train_sampler)
 
     start = datetime.now()
     total_step = len(train_loader)
@@ -83,13 +95,8 @@ def train(gpu, args):
             loss.backward()
             optimizer.step()
             if (i + 1) % 100 == 0 and gpu == 0:
-                print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'.format(
-                    epoch + 1,
-                    args.epochs,
-                    i + 1,
-                    total_step,
-                    loss.item())
-                )
+                print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'.format(epoch + 1, args.epochs, i + 1, total_step,
+                                                                         loss.item()))
     if gpu == 0:
         print("Training complete in: " + str(datetime.now() - start))
 
